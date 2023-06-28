cad735c660ccaeb25423cb0a0fadde299dabba44
umbertomorzone <127257161+umbertomorzone@users.noreply.github.com>
2023-06-28 18:41:49 +0200

    Update main_program.cpp

--- Raffinamento/main_program.cpp
+++ Raffinamento/main_program.cpp
@@ -4,18 +4,19 @@
 
 int main()
 {
-    int numeroIterazioni = 20;
-    int howMany = 20;
 //  int n = 5; // poi da cambiare: probabilmente è la size di vectsupp in heapsort
     vector<Project::Cell2D> vectSupp;
-
-
-
+    Project::TriangularMesh mesh;
     Project::Import();
-    for (int iter = 0; iter<numeroIterazioni; iter++) {
-        HeapSort(vectSupp);
-        for (int i = 0; i < howMany; i++) {
-            Bisect(vectSupp[i]);
+    Project::MatrAdiac MatriceAdiacenza = Project::MatrAdiac(mesh.vectt, mesh.vects);
+
+
+
+    for (int iter = 0; iter<20; iter++) {
+        HeapSort(vectSupp, Project::mesh.vectt);
+        for (int i = 0; i < 20; i++) {
+            Project::Cell2D& trisupp = vectSupp[i];
+            Bisect(trisupp, mesh.vectp, mesh.vects, mesh.vectt, MatriceAdiacenza.Matr);
         }
     }
 
