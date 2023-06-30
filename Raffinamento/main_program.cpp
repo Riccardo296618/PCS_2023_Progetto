6f43c5bad71c89da80d585a759045ffada956065
Riccardo296618 <134512042+Riccardo296618@users.noreply.github.com>
2023-06-30 22:48:02 +0200

    Update main_program.cpp

--- Raffinamento/main_program.cpp
+++ Raffinamento/main_program.cpp
@@ -10,32 +10,35 @@ using namespace Project;
 int main()
 {
 //  int n = 5; // poi da cambiare: probabilmente è la size di vectsupp in heapsort
-    unsigned int numbercell0D12 = 0;
-    vector<Project::Cell0D> vectp12;
-    unsigned int numbercell1D12 = 0;
-    vector<Project::Cell1D> vects12;
-    vector<double> LengthEdges12;
-    unsigned int numbercell2D12 = 0;
-    std::vector<vector<unsigned int>> LenghtMax12;
-    vector<Project::Cell2D> vectt12;
-
 
     vector<Project::Cell2D> vectSupp;
 
     //Project::TriangularMesh mesh;
-    Project::ImportCell0Ds(vectp12);
-    Project::ImportCell1Ds(vects12);
-    Project::ImportCell2Ds(vectt12);
+
+    unsigned int numbercell0D = 0;
+    extern vector<Project::Cell0D> vectp;
+    Project::ImportCell0Ds(vectp);
+
+    unsigned int numbercell1D = 0;
+    extern vector<Project::Cell1D> vects;
+    vector<double> LengthEdges;
+    Project::ImportCell1Ds(vects);
+
+    unsigned int numbercell2D = 0;
+    std::vector<vector<unsigned int>> LenghtMax;
+    extern vector<Project::Cell2D> vectt;
+    Project::ImportCell2Ds(vectt);
+
     //Project::MatrAdiac MatriceAdiacenza = Project::MatrAdiac(mesh.vectt, mesh.vects);
 
-    Project::TriangularMesh mesh = Project::TriangularMesh(numbercell0D12, vectp12, numbercell1D12, vects12, LengthEdges12, numbercell2D12, LenghtMax12, vectt12);
-    Project::MatrAdiac MatriceAdiacenza = Project::MatrAdiac(mesh.vectt, mesh.vects);
+    Project::TriangularMesh mesh = Project::TriangularMesh(numbercell0D, vectp, numbercell1D, vects, LengthEdges, numbercell2D, LenghtMax, vectt);
+    Project::MatrAdiac MatriceAdiacenza = Project::MatrAdiac(vectt, vects);
 
     for (int iter = 0; iter<20; iter++) {
-        HeapSort(vectSupp, mesh.vectt);
+        HeapSort(vectSupp, vectt);
         for (int i = 0; i < 20; i++) {
             Project::Cell2D& trisupp = vectSupp[i];
-            Bisect(trisupp, mesh.vectp, mesh.vects, mesh.vectt, MatriceAdiacenza.Matr);
+            Bisect(trisupp, vectp, vects, vectt, MatriceAdiacenza.Matr);
         }
     }
 
