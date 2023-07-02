f33b0087cbaefe64bbf3b1f39675f99dbae13ba6
Riccardo296618 <134512042+Riccardo296618@users.noreply.github.com>
2023-07-03 01:02:35 +0200

    Update main_program.cpp

--- Raffinamento/main_program.cpp
+++ Raffinamento/main_program.cpp
@@ -1,55 +1,36 @@
-#include <iostream>
-#include "Eigen/Eigen"
-#include <fstream>
-#include <algorithm>
-#include "cmath"
 #include "empty_class.hpp"
-#include "sorting.hpp"
 
-using namespace std;
-using namespace Eigen;
-using namespace Sorting;
-using namespace Project;
 
 
 int main()
 {
-//  int n = 5; // poi da cambiare: probabilmente è la size di vectsupp in heapsort
 
-    vector<Project::Cell2D> vectSupp;
+    vector<Cells::Cell2D> vectSupp;
 
-    //Project::TriangularMesh mesh;
+    unsigned int numbercell0D12 = 0;
+    vector<Project::Cell0D> vectp12;
+    unsigned int numbercell1D12 = 0;
+    vector<Project::Cell1D> vects12;
+    vector<double> LengthEdges12;
+    unsigned int numbercell2D12 = 0;
+    std::vector<vector<unsigned int>> LenghtMax12;
+    vector<Project::Cell2D> vectt12;
 
-    unsigned int numbercell0D = 0;
-    extern vector<Project::Cell0D> vectp;
-    Project::ImportCell0Ds(vectp);
 
-    unsigned int numbercell1D = 0;
-    extern vector<Project::Cell1D> vects;
-    vector<double> LengthEdges;
-    Project::ImportCell1Ds(vects);
+    Project::ImportCell0Ds();
+    Project::ImportCell1Ds();
+    Project::ImportCell2Ds();
+    Project::MatrAdiac MatriceAdiacenza = Project::MatrAdiac(mesh.vectt, mesh.vects);
 
-    unsigned int numbercell2D = 0;
-    std::vector<vector<unsigned int>> LenghtMax;
-    extern vector<Project::Cell2D> vectt;
-    Project::ImportCell2Ds(vectt);
-
-    //Project::MatrAdiac MatriceAdiacenza = Project::MatrAdiac(mesh.vectt, mesh.vects);
-
-    Project::TriangularMesh mesh = Project::TriangularMesh(numbercell0D, vectp, numbercell1D, vects, LengthEdges, numbercell2D, LenghtMax, vectt);
-    vector<vector<unsigned int>> Matr = MatrAdiac(vectt, vects);
-    //Project::MatrAdiac MatriceAdiacenza = Project::MatrAdiac(vectt, vects);
-
-    for (int iter = 0; iter<20; iter++) {
-        HeapSort(vectSupp, vectt);
+   for (int iter = 0; iter<20; iter++) {
+        HeapSort(vectSupp, Project::mesh.vectt);
         for (int i = 0; i < 20; i++) {
             Project::Cell2D& trisupp = vectSupp[i];
-            Bisect(trisupp, vectp, vects, vectt, Matr);   //, MatriceAdiacenza.Matr);
+            Bisect(trisupp, mesh.vectp, mesh.vects, mesh.vectt, MatriceAdiacenza.Matr);
         }
-    }
 
+   }
 
-//    empty.Show();
 
     return 0;
 }
